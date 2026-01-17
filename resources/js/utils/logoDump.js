8effb2cb1e7fa27be0689ae908c47e5788404c89
Muhammad Adil <adil.nusabakti117@gmail.com>
2026-01-17 20:31:26 +0800

    cliet terpercaya fix

---
+++ resources/js/utils/logoDump.js
@@ -0,0 +1,75 @@
+import Logos from "../assets/logo/Klien/logos.png";
+import Mtp from "../assets/logo/Klien/mtp.png";
+import Multi from "../assets/logo/Klien/multii.png";
+import Darma from "../assets/logo/Klien/darma.png";
+
+import Multilogis from "../assets/logo/Klien/multilogis.png";
+import Orbita from "../assets/logo/Klien/orbito.png";
+import Pulsatbang from "../assets/logo/Klien/pulsatbang.jpg";
+import Japfa from "../assets/logo/Klien/japfa.svg";
+
+import Rici from "../assets/logo/Klien/rici.png";
+import Roemah from "../assets/logo/Klien/roemah.png";
+import Vale from "../assets/logo/Klien/vale.png";
+import Suritani from "../assets/logo/Klien/suritani.png";
+
+import Zarindah from "../assets/logo/Klien/zarindah.png";
+import Pnup from "../assets/logo/Klien/pnup.png";
+import Transporter from "../assets/logo/Klien/transporter.png";
+import Stialan from "../assets/logo/Klien/stiaLan.png";
+
+import Altrak from "../assets/logo/Klien/altrak.png";
+import Sinar from "../assets/logo/Klien/sinar.jpg";
+import Lbh from "../assets/logo/Klien/lbh.png";
+import Pizza from "../assets/logo/Klien/pizza.png";
+
+import Amanah from "../assets/logo/Klien/amanah.png";
+import Kisel from "../assets/logo/Klien/kisel.png";
+import SaksiKorban from "../assets/logo/Klien/saksiKorban.png";
+import Bosowa from "../assets/logo/Klien/bosowa.png";
+
+import IlmuKesehatan from "../assets/logo/Klien/ilmuKesehatan.png";
+import DinsosGowa from "../assets/logo/Klien/dinsosGowa.jpg";
+import Tigadaun from "../assets/logo/Klien/tigadaun.jpg";
+import Logosconsul from "../assets/logo/Klien/logosConsul.png";
+import Solutiva from "../assets/logo/Klien/solutiva.png";
+
+const logoData = () => [
+    { id: 1, image: Logos, alt: "Logos" },
+    { id: 2, image: Mtp, alt: "Mtp" },
+    { id: 3, image: Multi, alt: "Multi" },
+    { id: 4, image: Darma, alt: "Darma" },
+
+    { id: 5, image: Multilogis, alt: "Multilogis" },
+    { id: 6, image: Orbita, alt: "Orbita" },
+    { id: 7, image: Pulsatbang, alt: "Pulsatbang" },
+    { id: 8, image: Japfa, alt: "Japfa" },
+
+    { id: 9, image: Rici, alt: "Rici" },
+    { id: 10, image: Roemah, alt: "Roemah" },
+    { id: 11, image: Vale, alt: "Vale" },
+    { id: 12, image: Suritani, alt: "Suritani" },
+
+    { id: 13, image: Zarindah, alt: "Zarindah" },
+    { id: 14, image: Pnup, alt: "Pnup" },
+    { id: 15, image: Transporter, alt: "Transporter" },
+    { id: 16, image: Stialan, alt: "Stialan" },
+
+    { id: 17, image: Altrak, alt: "Altrak" },
+    { id: 18, image: Sinar, alt: "Sinar" },
+    { id: 19, image: Lbh, alt: "Lbh" },
+    { id: 20, image: Pizza, alt: "Pizza" },
+
+    { id: 21, image: Amanah, alt: "Amanah" },
+    { id: 22, image: Kisel, alt: "Kisel" },
+    { id: 23, image: SaksiKorban, alt: "Saksi Korban" },
+    { id: 24, image: Bosowa, alt: "Bosowa" },
+
+    { id: 25, image: IlmuKesehatan, alt: "Ilmu Kesehatan" },
+    { id: 26, image: DinsosGowa, alt: "Dinsos Gowa" },
+    { id: 27, image: Tigadaun, alt: "Tiga Daun" },
+    { id: 28, image: Logosconsul, alt: "Logos Consul" },
+    { id: 29, image: Solutiva, alt: "Solutiva" },
+];
+
+export { logoData };
